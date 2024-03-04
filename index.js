70f358583bb71b4cbff3dc1f03f3ccdda90c12fc
GandhiRam2202 <gandhiramparthi@gmail.com>
2024-03-05 02:50:08 +0530

    update

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import express from 'express';
 import fs from 'fs';
 import { format } from 'date-fns'
 
-
 const app = express();
 const PORT = 4000;
 
-let toDay = format(new Date(), 'dd-MM-yyyy');
-let currentTime = format(new Date(), 'HH-mm-ss');
+// Function to convert UTC time to Indian Standard Time (IST)
+function convertToIST(utcDate) {
+    return new Date(utcDate.toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
+}
+
+let today = convertToIST(new Date());
+let toDay = format(today, 'dd-MM-yyyy');
+let currentTime = format(today, 'HH-mm-ss');
 const filePath = `TimeStamp/${toDay}.txt`
 
 app.get('/', (req, res) => {
@@ -17,14 +22,13 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on the port ${PORT}`);
 });
+
 app.get('/write', (req, res)=>{
     fs.writeFileSync(filePath, `${toDay}-${currentTime}`, 'utf8');
-    res.status(200).send(`<h1 style="text-align:center">File Writed Successfully : ${toDay}-${currentTime}</h1>`)
-    
-}) 
+    res.status(200).send(`<h1 style="text-align:center">File Written Successfully : ${toDay}-${currentTime}</h1>`);
+});
+
 app.get('/read', (req, res)=>{
     let data = fs.readFileSync(filePath, 'utf8');
-    res.status(200).send(`<h1 style="text-align:center">Data From File : <span style="text-align:center;color:red;">${data}</span></h1>`)
-
-}) 
-
+    res.status(200).send(`<h1 style="text-align:center">Data From File : <span style="text-align:center;color:red;">${data}</span></h1>`);
+});
