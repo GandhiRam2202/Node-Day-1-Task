889359841a48ea32073e2e472529cf039dad4b21
GandhiRam2202 <gandhiramparthi@gmail.com>
2024-03-05 01:02:58 +0530

    upate

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import { format } from 'date-fns'
 const app = express();
 const PORT = 4000;
 
-let toDay = format(new Date(), 'dd-MM-yyyy-HH-mm-ss');
+let toDay = format(new Date(), 'dd-MM-yyyy');
+let currentTime = format(new Date(), 'HH-mm-ss');
 
 app.get('/', (req, res) => {
     res.status(200).json({"message": 'Welcome to the Nodejs file system'});
@@ -18,12 +19,11 @@ app.listen(PORT, () => {
 app.get('/write', (req, res)=>{
     console.log(toDay);
     const filePath = `TimeStamp/${toDay}.txt`
-    fs.writeFileSync(filePath, `${toDay}`, 'utf8');
-    res.status(200).send(`<h1 style="text-align:center">File Writed Successfully : ${toDay}</h1>`)
+    fs.writeFileSync(filePath, `${toDay}-${currentTime}`, 'utf8');
+    res.status(200).send(`<h1 style="text-align:center">File Writed Successfully : ${toDay}-${currentTime}</h1>`)
     
 }) 
 app.get('/read', (req, res)=>{
-    console.log(toDay);
     const filePath = `TimeStamp/${toDay}.txt`
     let data = fs.readFileSync(filePath, 'utf8');
     res.status(200).send(`<h1 style="text-align:center">Data From File : <span style="text-align:center;color:red;">${data}</span></h1>`)
