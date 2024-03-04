e4b164b4b5446b5970440cff5ba0b2777a6508be
GandhiRam2202 <gandhiramparthi@gmail.com>
2024-03-05 01:11:21 +0530

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = 4000;
 
 let toDay = format(new Date(), 'dd-MM-yyyy');
 let currentTime = format(new Date(), 'HH-mm-ss');
+const filePath = `TimeStamp/${toDay}.txt`
 
 app.get('/', (req, res) => {
     res.status(200).json({"message": 'Welcome to the Nodejs file system'});
@@ -17,14 +18,11 @@ app.listen(PORT, () => {
     console.log(`Server running on the port ${PORT}`);
 });
 app.get('/write', (req, res)=>{
-    console.log(toDay);
-    const filePath = `TimeStamp/${toDay}.txt`
     fs.writeFileSync(filePath, `${toDay}-${currentTime}`, 'utf8');
     res.status(200).send(`<h1 style="text-align:center">File Writed Successfully : ${toDay}-${currentTime}</h1>`)
     
 }) 
 app.get('/read', (req, res)=>{
-    const filePath = `TimeStamp/${toDay}.txt`
     let data = fs.readFileSync(filePath, 'utf8');
     res.status(200).send(`<h1 style="text-align:center">Data From File : <span style="text-align:center;color:red;">${data}</span></h1>`)
 
